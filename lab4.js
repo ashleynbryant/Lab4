faf745634f676cb465c9b40fd24d09b34bfe74ec
ashleynbryant <57734245+ashleynbryant@users.noreply.github.com>
2019-11-13 19:32:09 -0500

    Add files via upload

---
+++ lab4.js
@@ -0,0 +1,10 @@
+﻿function validateForm() {
+    var x = document.forms["addListing"]["price"].value;
+    if(isNaN(x) || x < 1 || x > 10) {
+        alert("Please enter a numerical value");
+        return false;
+    }
+
+
+
+}
